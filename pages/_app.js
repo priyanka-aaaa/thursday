49a3409e9f6617a815804db409fcb02c8c5e0475
Priyanka <priyanka.calinfo500@gmail.com>
2022-07-15 14:51:05 +0530

    dfg

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,37 @@
 
-
-import '../styles/frontend.css'
-import '../styles/dashboard.css'
-import '../styles/my.css'
-import '../styles/responsive.css'
 import '../styles/style.css'
+import '../styles/dashboard.css'
+import '../styles/frontend.css'
+
+import '../styles/my.css'
 import '../styles/inquiry.css'
+import '../styles/responsive.css'
 
 
 
-import '../styles/style.css';
-import '../styles/dashboard.css';
-import '../styles/frontend.css';
-import '../styles/my.css';
-import '../styles/inquiry.css';
-import "../styles/responsive.css";
+
+
+import "@fontsource/roboto/100.css" 
+import "@fontsource/roboto/300.css" 
+import "@fontsource/roboto/400.css" 
+import "@fontsource/roboto/500.css" 
+import "@fontsource/roboto/700.css" 
+import "@fontsource/roboto/900.css" 
+import "@fontsource/rubik/300.css" 
+import "@fontsource/rubik/400.css" 
+import "@fontsource/rubik/500.css" 
+import "@fontsource/rubik/600.css" 
+import "@fontsource/rubik/700.css" 
+import "@fontsource/rubik/800.css" 
+import "@fontsource/titillium-web/200.css" 
+import "@fontsource/titillium-web/300.css" 
+import "@fontsource/titillium-web/400.css" 
+import "@fontsource/titillium-web/600.css" 
+import "@fontsource/titillium-web/700.css" 
+import "@fontsource/titillium-web/900.css" 
+import "@fontsource/source-sans-pro/400.css"
+import "@fontsource/source-sans-pro/600.css" 
+import "@fontsource/source-sans-pro/700.css" 
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
