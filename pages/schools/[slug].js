b075811fb951df54f10742f6f500d800b930b8d3
Priyanka <priyanka.calinfo500@gmail.com>
2022-07-23 10:52:59 +0530

    good

--- pages/schools/[slug].js
+++ pages/schools/[slug].js
@@ -432,54 +432,47 @@ const MyschoolDetails = (pageProps) => {
                 {/* <script type="application/ld+json">
                     {JSON.stringify(pageProps.parseData)}
                 </script> */}
-                <script type="application/ld+json">
-
-                    {JSON.stringify({
+                <script type='application/ld+json' dangerouslySetInnerHTML={{
+                    __html: `{
                         "@context": "https://schema.org",
                         "@type": "FAQPage",
                         "mainEntity": [{
-                            "@type": "Question",
-                            "name": "How old is Victoria University ?",
-                            "acceptedAnswer": {
-                                "@type": "Answer",
-                                "text": "Victoria University is a direct descendant of Footscray Technical School, which first opened its doors to students in February 1916. Local companies and residents sought to offer students an education that would prepare them for a future in a skilled industry and help them rise beyond the ranks of unskilled labourers."
-                            }
-                        },
-                        {
-                            "@type": "Question",
-                            "name": "Is Victoria University private or public?",
-                            "acceptedAnswer": {
-                                "@type": "Answer",
-                                "text": "Victoria University (VU or Vic Uni) is a public research university based in Melbourne, Victoria, Australia. It is one of only six dual-sector universities in Australia, providing courses in both higher education and Technical and Further Education (TAFE)."
-                            }
-                        },
-                        {
-                            "@type": "Question",
-                            "name": "What is the VU Global Leaders Scholarship ?",
-                            "acceptedAnswer": {
-                                "@type": "Answer",
-                                "text": "The scholarship supports new international students who plan to commence their higher education diploma, undergraduate or postgraduate (coursework) studies in 2022. A 20% tuition fee reduction for the entire standard duration of eligible courses (calculated from the date of commencement to the course end date in the letter of offer) is offered."
-                            }
-                        },
-                        {
-                            "@type": "Question",
-                            "name": "What is the ranking of Victoria University ?",
-                            "acceptedAnswer": {
-                                "@type": "Answer",
-                                "text": "In the Times Higher Education (THE) World University Rankings 2022 (and previously in 2017–21), VU was ranked in the top 2% of universities worldwide. In other THE rankings, VU is ranked in the world's top 100 universities under 50 years old, in the Times Higher Education Young University Rankings. Based on the United Nations Sustainable Development Goals, VU is ranked: #101-200."
-                            }
-                        },
-                        {
-                            "@type": "Question",
-                            "name": "How many campuses does Victoria University have?",
-                            "acceptedAnswer": {
-                                "@type": "Answer",
-                                "text": "Victoria University has campuses across Melbourne's CBD and western suburbs. International students can also choose to study at VU Sydney or partner institutions overseas. The flagship campus is located in Footscray."
-                            }
-                        }]
-                    })
-                    }
-                </script>
+                          "@type": "Question",
+                          "name": "What is the return policy?",
+                          "acceptedAnswer": {
+                            "@type": "Answer",
+                            "text": "<p>Most unopened items in new condition and returned within <b>90 days</b> will receive a refund or exchange. Some items have a modified return policy noted on the receipt or packing slip. Items that are opened or damaged or do not have a receipt may be denied a refund or exchange. Items purchased online or in-store may be returned to any store.</p><p>Online purchases may be returned via a major parcel carrier. <a href=http://example.com/returns> Click here </a> to initiate a return.</p>"
+                          }
+                        }, {
+                          "@type": "Question",
+                          "name": "How long does it take to process a refund?",
+                          "acceptedAnswer": {
+                            "@type": "Answer",
+                            "text": "We will reimburse you for returned items in the same way you paid for them. For example, any amounts deducted from a gift card will be credited back to a gift card. For returns by mail, once we receive your return, we will process it within 4–5 business days. It may take up to 7 days after we process the return to reflect in your account, depending on your financial institution's processing time."
+                          }
+                        }, {
+                          "@type": "Question",
+                          "name": "What is the policy for late/non-delivery of items ordered online?",
+                          "acceptedAnswer": {
+                            "@type": "Answer",
+                            "text": "<p>Our local teams work diligently to make sure that your order arrives on time, within our normaldelivery hours of 9AM to 8PM in the recipient's time zone. During  busy holiday periods like Christmas, Valentine's and Mother's Day, we may extend our delivery hours before 9AM and after 8PM to ensure that all gifts are delivered on time. If for any reason your gift does not arrive on time, our dedicated Customer Service agents will do everything they can to help successfully resolve your issue.</p><p><a href=https://example.com/orders/>Click here</a> to complete the form with your order-related question(s).</p>"
+                          }
+                        }, {
+                          "@type": "Question",
+                          "name": "When will my credit card be charged?",
+                          "acceptedAnswer": {
+                            "@type": "Answer",
+                            "text": "We'll attempt to securely charge your credit card at the point of purchase online. If there's a problem, you'll be notified on the spot and prompted to use another card. Once we receive verification of sufficient funds, your payment will be completed and transferred securely to us. Your account will be charged in 24 to 48 hours."
+                          }
+                        }, {
+                          "@type": "Question",
+                          "name": "Will I be charged sales tax for online orders?",
+                          "acceptedAnswer": {
+                            "@type": "Answer",
+                            "text":"Local and State sales tax will be collected if your recipient's mailing address is in: <ul><li>Arizona</li><li>California</li><li>Colorado</li></ul>"}
+                          }]
+                      }`}}
+                />
                 {/* <script type="application/ld+json">
                     {JSON.stringify(pageProps.objhello)}
                 </script> */}
