ca0583bf98a8b376d7632da369ba1eb09b6f2d24
Priyanka <priyanka.calinfo500@gmail.com>
2022-07-28 15:44:06 +0530

    xgvxc

--- components/Header.jsx
+++ components/Header.jsx
@@ -218,7 +218,7 @@ const Header = () => {
                   </nav>
                 </div>
 
-                <div className="homedropdown dropdown dropdown-menu-end my-account"><Link className="" href="/studentregister" data-bs-toggle="dropdown" title="Login or signup to access your dashboard"> <img layout="fill" src="/images/user-profile.png" alt="" unoptimized={true} loading="lazy" /></Link>
+                <div className="homedropdown dropdown dropdown-menu-end my-account"><Link className="" href="/studentregister" data-bs-toggle="dropdown" title="Login or signup to access your dashboard"><a><img layout="fill" src="/images/user-profile.png" alt="" unoptimized={true} loading="lazy" /></a> </Link>
                   <div className="dropdown-menu ">
                     <h5>Dashboard</h5>
                     <p>Login or signup to access your dashboard</p>
